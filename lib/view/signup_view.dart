58456cb473f2f75c434edc36d0ae0b9969168dc8
bibektako <133769273+bibektako@users.noreply.github.com>
2025-05-18 20:58:49 +0545

    signup page updated

--- lib/view/signup_view.dart
+++ lib/view/signup_view.dart
@@ -8,163 +8,208 @@ class SignupView extends StatefulWidget {
 }
 
 class _SignupView extends State<SignupView> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
   bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // App Logo
-            Image.asset(
-              'assets/image/loginimage.png',
-                width: double.infinity,
-                 height: 180,
-                fit: BoxFit.fitHeight,
-            ),
-
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // App Logo
+                Image.asset(
+                  'assets/image/loginimage.png',
+                    width: double.infinity,
+                     height: 180,
+                    fit: BoxFit.fitHeight,
+                ),
             
-
-            // Title
-            const Text(
-              "Create your account",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            const SizedBox(height: 8),
-
-            const Text(
-              "Create new account",
-              style: TextStyle(color: Colors.grey, fontSize: 18),
-            ),
-            const SizedBox(height: 32),
-
-            // Email Field
-            const Text(
-              "Email",
-              style: TextStyle(color: Colors.black, fontSize: 18),
-            ),
-            const SizedBox(height: 5),
-            TextFormField(
-              decoration:  InputDecoration(
-                prefixIcon: const Icon(Icons.email),
-                labelText: "yourmail@mail.com",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Colors.grey)
-                ),
-              ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 20),
-
-            // Password Field
-            const Text(
-              "Password",
-              style: TextStyle(color: Colors.black, fontSize: 18),
-            ),
-            const SizedBox(height: 5),
-            TextFormField(
-              obscureText: _obscureText,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.key),
-                labelText: "Password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Colors.grey)
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscureText ? Icons.visibility_off : Icons.visibility,
+                
+            
+                // Title
+                const Text(
+                  "Create your account",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontStyle: FontStyle.italic,
                   ),
-                  onPressed: () {
-                    setState(() {
-                      _obscureText = !_obscureText;
-                    });
+                ),
+                const SizedBox(height: 8),
+            
+                const Text(
+                  "Create new account",
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                ),
+                const SizedBox(height: 32),
+            
+                // Email Field
+                const Text(
+                  "Email",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+                const SizedBox(height: 5),
+                TextFormField(
+                  decoration:  InputDecoration(
+                    prefixIcon: const Icon(Icons.email),
+                    labelText: "yourmail@mail.com",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.grey)
+                    ),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if(value == null || value.isEmpty){
+                      return "Enter the email";
+                    }
+                    return null;
                   },
                 ),
-              ),
-            ),
-            const SizedBox(height: 20),
-
-            // Confirm Password Field
-            const Text(
-              "Confirm Password",
-              style: TextStyle(color: Colors.black, fontSize: 18),
-            ),
-            const SizedBox(height: 5),
-            TextFormField(
-              obscureText: _obscureText,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.key),
-                labelText: "Conform Password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Colors.grey)
+                const SizedBox(height: 20),
+            
+                // Password Field
+                const Text(
+                  "Password",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
                 ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscureText ? Icons.visibility_off : Icons.visibility,
+                const SizedBox(height: 5),
+                TextFormField(
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.key),
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.grey)
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility_off : Icons.visibility,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
                   ),
-                  onPressed: () {
-                    setState(() {
-                      _obscureText = !_obscureText;
-                    });
+                  validator: (value) {
+                    if(value == null || value.isEmpty){
+                      return "Enter password";
+                    }else if(value.length < 6){
+                      return "Password must be at least 6 character";
+                    }
+                    return null;
                   },
                 ),
-              ),
-            ),
-            const SizedBox(height: 24),
-
-            // Register Button
-            SizedBox(
-              width: double.infinity,
-              height: 48,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueAccent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+                const SizedBox(height: 20),
+            
+                // Confirm Password Field
+                const Text(
+                  "Confirm Password",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+                const SizedBox(height: 5),
+                TextFormField(
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.key),
+                    labelText: "Confirm Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.grey)
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility_off : Icons.visibility,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
+                  ),
+                  validator:(value) {
+                    if(value == null || value.isEmpty){
+                      return "Confirm password cannot be empty";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 24),
+            
+                // Register Button
+                SizedBox(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()){
+                        String email = _emailController.text.trim();
+                        String password = _passwordController.text.trim();
+                        String confirmPassword = _confirmPasswordController.text.trim();
+            
+                        if(password != confirmPassword){
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text("Password and Confirm Password does't match "),
+                            behavior: SnackBarBehavior.floating,
+                            ));
+                        }else if(confirmPassword == password && email == "admin"){
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Registration success")));
+                        }
+                      }
+                    },
+                    child: const Text("Register",
+                    style: TextStyle(color: Colors.white),),
                   ),
                 ),
-                onPressed: () {},
-                child: const Text("Register",
-                style: TextStyle(color: Colors.white),),
-              ),
-            ),
-            const SizedBox(height: 16),
-
-            // Google Button
-            SizedBox(
-              width: double.infinity,
-              height: 48,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  side: const BorderSide(color: Colors.grey),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+                const SizedBox(height: 16),
+            
+                // Google Button
+                SizedBox(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(color: Colors.grey),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Register with Google",
+                      style: TextStyle(color: Colors.black),
+                    ),
                   ),
                 ),
-                onPressed: () {},
-                child: const Text(
-                  "Register with Google",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
