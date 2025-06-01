75168274fb2ff97f2489682e9f2ad48c8767de16
bibektako <133769273+bibektako@users.noreply.github.com>
2025-06-01 14:26:34 +0545

    New font added

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -6,7 +6,6 @@ class LoginView extends StatefulWidget {
 
   @override
   State<LoginView> createState() => _LoginView();
-  
 }
 
 class _LoginView extends State<LoginView> {
@@ -18,7 +17,7 @@ class _LoginView extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(backgroundColor: Colors.white),
+      appBar: AppBar(),
       body: Form(
         key: _formKey,
         child: Padding(
@@ -27,10 +26,11 @@ class _LoginView extends State<LoginView> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Image.asset('assets/image/loginimage.png', 
-                width: double.infinity,
-                height: 200,
-                fit: BoxFit.fitHeight
+                Image.asset(
+                  'assets/image/loginimage.png',
+                  width: double.infinity,
+                  height: 200,
+                  fit: BoxFit.fitHeight,
                 ),
                 SizedBox(height: 12),
                 Container(
@@ -56,7 +56,10 @@ class _LoginView extends State<LoginView> {
                   ),
                 ),
                 SizedBox(height: 32),
-                Text("Email", style: TextStyle(color: Colors.black, fontSize: 18)),
+                Text(
+                  "Email",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
                 TextFormField(
                   controller: _emailController,
                   decoration: InputDecoration(
@@ -64,22 +67,19 @@ class _LoginView extends State<LoginView> {
                     hintText: "Yourmail@mail.com",
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(color: Colors.grey)
+                      borderSide: BorderSide(color: Colors.grey),
                     ),
                   ),
                   validator: (value) {
-                    if (value == null || value.isEmpty){
-            
+                    if (value == null || value.isEmpty) {
                       return "Please enter email";
                     }
-            
+
                     // else if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)){
                     //   return "Please enter a valid email";
                     // }
                     return null;
                   },
-                        
-                  keyboardType: TextInputType.numberWithOptions(),
                 ),
                 SizedBox(height: 20),
                 Text(
@@ -95,7 +95,7 @@ class _LoginView extends State<LoginView> {
                     hintText: "Input your password",
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(color: Colors.grey)
+                      borderSide: BorderSide(color: Colors.grey),
                     ),
                     suffixIcon: IconButton(
                       icon: Icon(
@@ -109,10 +109,9 @@ class _LoginView extends State<LoginView> {
                     ),
                   ),
                   validator: (value) {
-                    if(value == null || value.isEmpty){
+                    if (value == null || value.isEmpty) {
                       return "Enter the password";
-            
-                    }else if(value.length < 5 ){
+                    } else if (value.length < 5) {
                       return "Password must be at least 5 characters";
                     }
                     return null;
@@ -149,31 +148,34 @@ class _LoginView extends State<LoginView> {
                       ),
                     ),
                     onPressed: () {
-                      if(_formKey.currentState!.validate()){
+                      if (_formKey.currentState!.validate()) {
                         String email = _emailController.text.trim();
                         String password = _passwordController.text.trim();
-                        if(email == 'admin' && password == 'admin123'){
+                        if (email == 'admin' && password == 'admin123') {
                           ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text("Login success"),
-                            behavior: SnackBarBehavior.floating,
-            
-                          ));
-                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => DashboardView()));
-                        }else{
-                         ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text("Login failed"),
-                          behavior: SnackBarBehavior.floating,
-                          backgroundColor: Colors.blue,
-            
-                          ));
+                            SnackBar(
+                              content: Text("Login success"),
+                              behavior: SnackBarBehavior.floating,
+                            ),
+                          );
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DashboardView(),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("Login failed"),
+                              behavior: SnackBarBehavior.floating,
+                              backgroundColor: Colors.blue,
+                            ),
+                          );
                         }
                       }
                     },
-                    child: Text("Login",
-                    style: TextStyle(
-                      color: Colors.white
-                    ),
-                    ),
+                    child: Text("Login", style: TextStyle(color: Colors.white)),
                   ),
                 ),
                 SizedBox(height: 16),
@@ -187,16 +189,14 @@ class _LoginView extends State<LoginView> {
                         borderRadius: BorderRadius.circular(8),
                       ),
                     ),
-                    onPressed: () {
-                      
-                    },
-                    
+                    onPressed: () {},
+
                     child: Text("Login with Google"),
                   ),
                 ),
-                    
+
                 SizedBox(height: 50),
-                    
+
                 SizedBox(
                   child: RichText(
                     textAlign: TextAlign.center,
